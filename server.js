39b66eb4a87be2d3a1a31bb2c49d20efe6236dbe
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-10 18:55:05 -0500

    connect to db

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const colors = require('colors')
 const { appendFile } = require('fs')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
