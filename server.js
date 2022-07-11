543ab9a14c9baae1fdad51a0c5d4b38f81f47deb
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-10 19:20:22 -0500

    user set up

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.use(errorHandler)
 
 app.listen(PORT, () => {
