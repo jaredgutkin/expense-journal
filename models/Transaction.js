c58e16d27eb4d9b956cbb8ed9b698417f790fa73
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-10 19:01:01 -0500

    basic models added

---
+++ models/Transaction.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const TransactionSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    text: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add some text']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'please add a positive or negative number']
+    },
+    createdAt : {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Transaction', TransactionSchema)
