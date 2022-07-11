bb9c2cb551d236692e926a8ab17a6a49b9b26548
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-10 19:09:38 -0500

    middleware setup

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express = require('express')
 const colors = require('colors')
 const { appendFile } = require('fs')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
 connectDB()
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`.yellow)
